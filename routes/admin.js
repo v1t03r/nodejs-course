d5ef7c2c19b47d350291d7bd04558dcd8e796318
“Victor <victor.pacheco@gmail.com>
2021-01-26 17:43:10 -0300

    update edit-produt data

--- routes/admin.js
+++ routes/admin.js
@@ -15,7 +15,8 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-// /admin/edit-product => GET
 router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
